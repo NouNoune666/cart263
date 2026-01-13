91093ae0302b5f68b087d12d47b06688a1664dfa
NouNoune666 <barretteamelie@gmail.com>
2026-01-13 14:51:45 -0500

    Initial commit

---
+++ tasks/task1/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function setup() {
+    console.log("go");
+    createCanvas(300, 300)
+}
+
+function draw() {
+    background(0);
+
+    //Q1
+    // push();
+    // noStroke();
+    // fill('pink');
+    // ellipse(10, 20, 10)
+    // pop();
+
+    // push();
+    // noStroke();
+    // fill('purple');
+    // ellipse(30, 40, 20)
+    // pop();
+
+    // push();
+    // noStroke();
+    // fill('teal');
+    // ellipse(60, 70, 30)
+    // pop();
+
+}
