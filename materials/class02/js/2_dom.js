58ef3019aa82f44ee2b36f4a8449b957ab9aef3b
NouNoune666 <barretteamelie@gmail.com>
2026-01-21 12:27:52 -0500

    Class notes

--- materials/class02/js/2_dom.js
+++ materials/class02/js/2_dom.js
@@ -1,6 +1,38 @@
 window.onload = setup
+
 function setup() {
-    console.log("running setup");
-    document.getElementById()
-    console.log(document.querySelector("#one"));
+    // console.log("running setup");
+    // console.log(document.getElementById("one"));
+    // console.log(document.querySelector("#one"));
+
+    // let allDivs = document.getElementsByTagName("div");
+    // console.log(allDivs[1]);
+    // console.log(allDivs.length);
+
+    // console.log(document.getElementsByTagName("div"));
+    // console.log(document.querySelectorAll("div p"));
+
+    // console.log(document.getElementById("two").textContent);
+
+    // console.log(document.querySelector("#five").getAttribute("id"));
+    // console.log(document.querySelector("#five").getAttribute("class"));
+
+    // console.log(document.querySelector("#two").classList);
+
+    // console.log(document.querySelector("#one").style.background);
+
+
+    // MODIFYING DOM ELEMENTS
+
+    // console.log(document.querySelectorAll("span")[0].parentElement.parentElement)
+    // console.log(document.querySelector(".wrapper_flex_box").children[0])
+
+
+    // document.getElementById("two").children[0].innerHTML = "<h3> Hello Js </h3>";
+
+    let childrenOfTwo = document.getElementById("two").children;
+    for (let i = 0; i < childrenOfTwo.length; i++) {
+        childrenOfTwo[i].innerHTML = "<h3> Hello Js </h3>"
+    }
+
 }
---
+++ materials/class02/notes.md
@@ -0,0 +1,10 @@
+- document object model = DOM
+- document.write (document is an object, built  the browser)
+- in vanilla javascript the library is IN the browser
+
+- Elements (ex: head, body, etc.)
+- propreties (ex: for image (src), for p (div or id) )
+
+- window.onload = setup the function 'setup' will run when the window has loaded
+
+- Ways to access element ID : .getElementByID or .getElementsByTagName or .querySelector
