2d1b7641260c3504c6f3767c598cdf2c06c281d5
NouNoune666 <barretteamelie@gmail.com>
2026-02-17 15:47:25 -0500

    class 06

---
+++ materials/class06/js/Bird.js
@@ -0,0 +1,66 @@
+
+class Bird extends Animal {
+    // Create a new bird object that moves to the right
+    constructor(x, y, width, height) {
+        super(x, y, width, height);
+        this.vx = Math.random() * 5 + 1;
+        this.vy = 0;
+        this.animalBody = document.createElement("div");
+
+
+        this.originalY = this.y;
+        //ONLY in the Bird class : new variables
+        this.angle = 0;
+        this.sleepiness = 0.1;
+
+    }
+
+    move() {
+        this.y = this.originalY + Math.sin(this.angle) * 8
+        this.angle += 0.05;
+        this.veer;
+        super.move();
+    }
+
+
+    veer() {
+        let r = Math.random();
+        //console.log("in veer "+r)
+        if (r < this.sleepiness) {
+            this.vy += randomRange(-.1, .1);
+        }
+    }
+
+    // Overriding the superclass' wrap() method!
+    wrap() {
+        // NEW! Call the superclass (Animal) version of wrap()
+        // This will handle wrapping on the x axis for us.
+        // We use "super" to access methods in the SUPERclass (Animal)
+        // So this calls the Animal version of wrap() after we make the specific changes 
+        // in the SUBclass.
+
+        if (this.x > window.innerWidth) {
+            //reset
+            this.vy = 0;
+        }
+        super.wrap();
+    }
+
+    // Display the bird as a ellipse
+    renderAnimal() {
+        super.renderAnimal();
+
+        this.animalBody.style.backgroundColor = `rgb(106, 90, 205)`;
+        //add to the DOM
+        document.getElementsByClassName("sky")[0].appendChild(this.animalBody);
+    }
+
+
+}
+
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
--- materials/class06/js/garden.js
+++ materials/class06/js/garden.js
@@ -1,6 +1,20 @@
-window.onload = function (){
-// Our garden
-let garden = {
+window.onload = function () {
+  // Our garden
+  let garden = {
+
+    // NEW: Properties to track our birds
+    // An array to store the individual birds
+    birds: [],
+    // How many birds in the garden
+    numBirds: 20,
+
+    // NEW: Properties to track our dogs
+    // numDogs controls how many dogs will be created
+    numDogs: 10,
+    // dogs array will store all our Dog objects (same pattern as flowers[])
+    dogs: [],
+
+
     /*grass object */
     grass: {
       // The color of the grass (background)
@@ -12,7 +26,7 @@ let garden = {
       //the grass element
       grassDiv: document.createElement("div"),
     },
- 
+
     /*sky object */
     sky: {
       // The color of the sky (background)
@@ -26,7 +40,7 @@ let garden = {
     },
   };
   // new  sun instancce
-  let sun =  new Sun(10,10,{r: 240, g: 206,b: 83})
+  let sun = new Sun(10, 10, { r: 240, g: 206, b: 83 })
 
   function createAndRenderTheGarden() {
     /* note how we use dot notation....*/
@@ -41,11 +55,104 @@ let garden = {
     garden.grass.grassDiv.classList.add("grass");
     garden.grass.grassDiv.style.background = `rgb(${garden.grass.grassColor.r},${garden.grass.grassColor.g},${garden.grass.grassColor.b})`;
     document.getElementsByTagName("main")[0].appendChild(garden.grass.grassDiv);
+  }
 
-    
+  function createDogs() {
+    // Create the correct number of dogs and put them in our array
+    for (let i = 0; i < garden.numDogs; i++) {
+      let x = Math.random() * window.innerWidth;
+      let y = Math.random() * 100;
+      let dog = new Dog(x, y, 15, 15);
+      garden.dogs.push(dog);
+    }
+  }
+
+  function createBirds() {
+
+    //NEW: create some birds
+    for (let i = 0; i < garden.numBirds; i++) {
+      let x = Math.random() * window.innerWidth;
+      let y = Math.random() * 100;
+      let bird = new Bird(x, y, 15, 15);
+      garden.birds.push(bird);
+    }
+
 
   }
+
+  function renderAnimals() {
+    for (let i = 0; i < garden.dogs.length; i++) {
+      // Get the dog at index i from the array
+      let dog = garden.dogs[i];
+      // Call that dog's renderAnimal() method - defined in Dog.js
+      // This adds the dog's div to the grass element on the page
+      dog.renderAnimal();
+    }
+
+    // Go through all the birds and move, wrap, and display them
+    for (let i = 0; i < garden.birds.length; i++) {
+      let bird = garden.birds[i];
+      bird.renderAnimal();
+    }
+  }
+
+  function updateGarden() {
+    for (let i = 0; i < garden.dogs.length; i++) {
+      let dog = garden.dogs[i];
+      // Call move() - updates the dog's x/y position based on its velocity
+      dog.move();
+      // Call wrap() - if dog went off the right edge, teleport it back to the left
+      dog.wrap();
+    }
+
+    // Go through all the birds and move, wrap, and display them
+    for (let i = 0; i < garden.birds.length; i++) {
+      let bird = garden.birds[i];
+      bird.move();
+      bird.wrap();
+    }
+
+    // if the first dog is set to jump
+    if (garden.dogs[0].isjumping === true) {
+      console.log("jump")
+      garden.dogs[0].updateJump();
+      for (let i = 0; i < garden.birds.length; i++) {
+        //try to catch any bird
+        garden.dogs[0].catchBird(garden.birds[i])
+      }
+
+    }
+
+    // NEW: requestAnimationFrame is what creates the animation loop
+    // It tells the browser: "run updateGarden() again before the next screen refresh"
+    // This happens ~60 times per second, making the dogs appear to move smoothly
+    window.requestAnimationFrame(updateGarden)
+
+  }
+
+
+  createDogs();
+  createBirds();
   createAndRenderTheGarden();
+  renderAnimals();
+  window.requestAnimationFrame(updateGarden);
+
+  // new jump on key press
+  window.addEventListener("keydown", function (e) {
+    //set up to allow got "0" to jump 
+    if (e.code === "Space") {
+      //prevent default behaviour of the space bar
+      e.preventDefault()
+      //check if the dog is already jumping
+      if (garden.dogs[0].isjumping === false) {
+        garden.dogs[0].jump()
+      }
+
+    }
+  })
 }
 
-  
+
+
+
+
