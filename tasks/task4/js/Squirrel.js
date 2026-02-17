a6fb111599ae01473157ed03169c02c72de4189f
NouNoune666 <barretteamelie@gmail.com>
2026-02-17 16:51:17 -0500

    Did first 3 steps of task 4

---
+++ tasks/task4/js/Squirrel.js
@@ -0,0 +1,40 @@
+class Squirrel {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.squirrelBody = document.createElement("div");
+        this.color = color;
+    }
+
+    renderSquirrel() {
+        this.squirrelBody.classList.add("squirrel");
+        this.squirrelBody.style.width = this.width + "px";
+        this.squirrelBody.style.height = this.height + "px";
+        this.squirrelBody.style.left = this.x + "px";
+        this.squirrelBody.style.top = this.y + "px";
+        //add to the DOM
+        document.getElementsByClassName("grass")[0].appendChild(this.squirrelBody);
+    }
+
+
+    // Move the squirrel according to its velocity
+    move() {
+        this.x += this.vx;
+        this.y += this.vy;
+        //update the actual div...
+        this.squirrelBody.style.left = this.x + "px";
+        this.squirrelBody.style.top = this.y + "px";
+    }
+
+    // Wrap the squirrel if it reaches the right edge
+    wrap() {
+        if (this.x > window.innerWidth) {
+            this.x -= window.innerWidth;
+        }
+    }
+
+
+
+}
