e693e96f67f0bd90f006311a2d2f0f511c4eebea
NouNoune666 <barretteamelie@gmail.com>
2026-03-05 16:28:31 -0500

    done with html and java 2!

--- projects/projectOne/js/htmljava2.js
+++ projects/projectOne/js/htmljava2.js
@@ -12,8 +12,7 @@ function start() {
     ];
 
 
-    // click event(something happens to a square)
-
+    // click event on box A
     let colorIndex = 0;
     let boxA = document.querySelector("#boxA");
 
@@ -21,28 +20,108 @@ function start() {
         console.log("click")
 
         // changing border color with array
-        boxA.style.borderColor = borderColors[colorIndex]
+        this.style.borderColor = borderColors[colorIndex]
         colorIndex = (colorIndex + 1) % borderColors.length  // loops back to 0
-        console.log(colorIndex)
+        // console.log(colorIndex)
 
         // changing background color with HSL (lightness)
         let randomL = Math.ceil(Math.random() * 100)  // 0 to 100
-        boxA.style.backgroundColor = `hsl(324, 59%, ${randomL}%)`;
-        console.log(randomL)
+        this.style.backgroundColor = `hsl(324, 59%, ${randomL}%)`;
+        // console.log(randomL)
     });
 
 
 
-    // mouse move(something moves)
+    // keyboard event on box B
+    let boxB = document.querySelector("#boxB");
 
-    // key space(becomes bigger)
+    let speed = 10;
+    window.addEventListener("keydown", function (e) {
 
-    // key enter - custom bool active, inactive
+        console.log(e.key)
+        let currentY = parseInt(boxB.style.left);
+        let currentX = parseInt(boxB.style.top);
+        // console.log(currentY);
+        // console.log(currentX);
 
-    // drag event
+        if (e.key === "a") {
+            boxB.style.left = currentY - speed + "px"
+        }
+        else if (e.key === "d") {
+            boxB.style.left = currentY + speed + "px"
+        }
+        else if (e.key === "w") {
+            boxB.style.top = currentX - speed + "px"
+        }
+        else if (e.key === "s") {
+            boxB.style.top = currentX + speed + "px"
+        }
+    })
 
-    // make dot or image at location
+    // keyboard event on box C
+    let growth = 50;
+    let boxC = document.querySelector("#boxC")
 
+    window.addEventListener("keydown", function (e) {
+
+        if (e.key === "Shift") {
+
+            let boxCWidth = parseInt(getComputedStyle(document.querySelector("#boxC")).width);
+            let boxCHeight = parseInt(getComputedStyle(document.querySelector("#boxC")).height);
+
+            // console.log(boxCWidth);
+            // console.log(boxCHeight);
+
+            boxC.style.width = boxCWidth + growth + "px";
+            boxC.style.height = boxCHeight + growth + "px";
+        }
+
+        if (e.key === "Backspace") {
+
+            let boxCWidth = parseInt(getComputedStyle(document.querySelector("#boxC")).width);
+            let boxCHeight = parseInt(getComputedStyle(document.querySelector("#boxC")).height);
+
+
+            boxC.style.width = boxCWidth - growth + "px";
+            boxC.style.height = boxCHeight - growth + "px";
+        }
+    })
+
+
+
+    // custom bool hidden
+    let button = document.querySelectorAll(".box")[3];
+    let pingu = document.querySelector("img");
+    console.log(pingu)
+    button.addEventListener("click", function (e) {
+        // Check current state
+        pingu.classList.toggle("active-class")
+    });
+
+
+    // mouse event - drawing dots
+    let canvas = document.querySelector("#allBoxes");
+
+    let width = 20;
+    let height = 20;
+
+    canvas.addEventListener("mousemove", function (e) {
+        // create new div, append to parent "div", set style
+        let parent = document.querySelector("#allBoxes ");
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("dot");
+        parent.appendChild(newDiv);
+        newDiv.style.left = `${e.clientX}px`;
+        newDiv.style.top = `${e.clientY}px`;
+        newDiv.style.width = width + "px";
+        newDiv.style.height = height + "px";
+
+        // dot color
+        r = Math.random() * 255;
+        g = Math.random() * 255;
+        b = Math.random() * 255;
+        newDiv.style.backgroundColor = `rgba(${r}, ${g}, ${b})`;
+    });
 
 
 }
